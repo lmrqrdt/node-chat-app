a5b8069570eff82f7b9515147a0139ec6dbd5ae8
Larry Marquardt <lmrqrdt@gmail.com>
2018-04-28 12:25:24 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: 'Jessica',
-    text: 'Hey, this is Larry'
-  })
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
@@ -24,14 +24,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Larry',
-    text: 'Hey, what is going on?',
-    createdAt: 123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
